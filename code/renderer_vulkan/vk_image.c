4d4e425911a7c244b0f65186d14f56bb1e44d31d
metaq3 <ruslanbez2006@gmail.com>
2026-05-28 14:18:13 +0500

    chore: code sanity improvements

--- code/renderer_vulkan/vk_image.c
+++ code/renderer_vulkan/vk_image.c
@@ -509,7 +509,7 @@ image_t* R_CreateImage( const char *name, unsigned char* pic, const uint32_t wid
     }
 
 
-    ri.Printf( PRINT_ALL, " Create Image: %s\n", name);
+    ri.Printf( PRINT_DEVELOPER, " Create Image: %s\n", name);
     
     // Create image_t object.
 
@@ -1081,7 +1081,7 @@ void R_InitImages( void )
 static void vk_destroySingleImage( image_t* pImg )
 {
 
-   	ri.Printf(PRINT_ALL, " Destroy Image: %s \n", pImg->imgName); 
+   	ri.Printf(PRINT_DEVELOPER, " Destroy Image: %s \n", pImg->imgName); 
     if(pImg->descriptor_set != VK_NULL_HANDLE)
     {   
         //To free allocated descriptor sets
